3f59a592253641f7beb8603b5f2674325aeb5cdb
Brian Guarraci <brian.guarraci@gmail.com>
2021-12-21 15:05:35 -0700

    add slow but interesting example

--- push/mgr/mdc.py
+++ push/mgr/mdc.py
@@ -7,19 +7,37 @@ import sys
 m = QueueManager(address=('', int(sys.argv[1])), authkey=b'password')
 m.connect()
 
+# while jq.qsize() > 0:
+#     jq.get_sync()
+# while rq.qsize() > 0:
+#     rq.get_sync()
+
+class BatchProcess:
+    def __init__(self, _jq=None, _rq=None):
+        global job_queue
+        global result_queue
+        self.jq = _jq or job_queue
+        self.rq = _rq or result_queue
+
+    def apply(self):
+        results = []
+        for i in range(100):
+            self.jq.put(('add', i, i*2))
+            self.jq.put(('sub', i*2, i))
+        i = 0
+        while i < 100:
+            rv = self.rq.get_sync()
+            if rv is None:
+                continue
+            results.append(rv)
+            i += 1
+        return results
+
+dl = m.apply_lambda()
+r = dl.apply(src=dill.dumps(BatchProcess))
+print(r)
+
 jq = m.get_job_queue()
 rq = m.get_result_queue()
-
-while rq.qsize() > 0:
-    rq.get_sync()
-
-jq.put_sync(('add', 5, 6))
-jq.put_sync(('sub', 10, 2))
-while rq.qsize() < 2:
-    time.sleep(0.1)
-rv = rq.get_sync()
-print(f"result queue: {rv}")
-rv = rq.get_sync()
-print(f"result queue: {rv}")
-jq.put_sync(('exit',))
-jq.put_sync(('exit',))
+bp = BatchProcess(jq, rq)
+print(bp.apply())
