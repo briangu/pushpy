5fb228ddb97fd1a6b15b96d2c67de571d8a048f0
Brian Guarraci <brian.guarraci@gmail.com>
2022-01-03 16:42:40 -0700

    add todo

--- push/mgr/ts_c_r.py
+++ push/mgr/ts_c_r.py
@@ -14,3 +14,33 @@ print(m.repl_ts().flatten())
 dt = m.local_tasks()
 r = dt.apply(src=dill.dumps(lambda *args, **kwargs: repl_ts.flatten()))
 print(r)
+
+# import dill
+#
+# from push.mgr.push_manager import PushManager
+# import sys
+#
+# m = PushManager(address=('', int(sys.argv[1])), authkey=b'password')
+# m.connect()
+#
+#
+# class BatchProcess:
+#     def __init__(self, _ts=None):
+#         global repl_ts
+#         self.ts = _ts or repl_ts
+#
+#     def apply(self):
+#         flat_ts = self.ts.flatten()
+#         print(flat_ts)
+#         return flat_ts
+#
+#
+# ts = m.repl_ts()
+# dt = m.local_tasks()
+# r = dt.apply(dill.dumps(BatchProcess))
+# print(r)
+#
+# print(m.repl_ts().flatten())
+#
+# r = dt.apply(src=dill.dumps(lambda *args, **kwargs: repl_ts.flatten()))
+# print(r)
