758931b0dcc21a0292d3e5e8ab7ebf74d59eb823
Brian Guarraci <brian.guarraci@gmail.com>
2022-01-03 15:27:26 -0700

    make loader example use imports

--- push/mgr/ts_c_loader.py
+++ push/mgr/ts_c_loader.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import dill
 
@@ -49,3 +50,24 @@ repl_code_store.update({
 
 commands = ['add', 'add', 1, 2, 'mul', 3, 4]
 print(local_tasks.apply("interpreter.Interpreter", commands)[0])
+
+
+class AdderTask:
+    def apply(self, control):
+        print("my_adder_task daemon here! 1")
+
+        import random
+        import time
+
+        while control.running:
+            commands = ['add', 'add', random.randint(0, 10), random.randint(0, 10), 'mul', random.randint(0, 10), random.randint(0, 10)]
+            print(local_tasks.apply("interpreter.Interpreter", commands)[0])
+            time.sleep(1)
+
+repl_code_store.set("my_adder_task", dill.dumps(AdderTask), sync=True)
+
+dt = m.local_tasks()
+dt.stop("mat")
+dt.run("daemon", src="kvstore:my_adder_task", name="mat")
+
+time.sleep(300)
