c94248fd1677b63a8516197c6dbb79e269a1d347
Brian Guarraci <brian.guarraci@gmail.com>
2022-01-05 14:09:48 -0700

    improve repl

--- push/push_repl.py
+++ push/push_repl.py
@@ -6,72 +6,78 @@ import dill
 
 from push.push_manager import PushManager
 
+push_managers = dict()
+default_host = sys.argv[1]
 
 
+def host_to_address(host):
+    p = host.split(":")
+    return (p[0], int(p[1])) if len(p) == 2 else ('', int(p[-1]))
 
-# async def hello_cmd(dt, line):
-#     p = line.split(" ")
-#     expect_response = True
-#     if p[0] == '=':
-#         p = [p[0], " ".join(p[1:])]
-#         expect_response = False
-#     elif p[0] == 'c':
-#         p = [p[0], " ".join(p[1:])]
-#     elif p[0].startswith("!"):
-#         x = line[1:]
-#         p = ['l', f"os.system(\"{x}\")"]
-#     else:
-#         p = ['l', " ".join(p)]
-#     try:
-#         s, tx, tx2 = await process_command(websocket, p, expect_response=expect_response)
-#         print(tx, tx2, np.round(tx - tx2, 4))
-#         print(process_response(s))
-#     except Exception as e:
-#         traceback.print_exc()
-#         print(e)
-#         pass
+
+def connect_to_host(host):
+    if host not in push_managers:
+        m = PushManager(address=host_to_address(host), authkey=b'password')
+        m.connect()
+        push_managers[host] = m
+    return push_managers[host]
+
+
+def host_exec_cmd(dt, cmd):
+    code = compile(cmd, str(uuid.uuid4()), "eval")
+    return dt.apply(dill.dumps(code))
+
+
+def hello_cmd(cmd):
+    cmd = cmd[1:]
+    return cmd
 
 
 async def hello(host):
-    m = PushManager(address=('', int(host)), authkey=b'password')
-    m.connect()
+    m = connect_to_host(host)
     dt = m.local_tasks()
     print(f"{host} >>> ", end='')
     sys.stdout.flush()
     for line in sys.stdin:
         cmd = line.rstrip()
-        code = compile(cmd, str(uuid.uuid4()), "eval")
-        r = dt.apply(dill.dumps(code))
-        print(r)
+        cmd = hello_cmd(cmd) if cmd.startswith("!") else cmd
+        if len(cmd) > 0:
+            r = host_exec_cmd(dt, cmd)
+            print(r)
         print(f"{host} >>> ", end='')
         sys.stdout.flush()
     print()
     print(f"leaving {host}")
 
 
-async def sac_cmd(line):
-    if line.startswith("@"):
-        host = line[1:]
+async def sac_cmd(cmd):
+    if cmd.startswith("@"):
+        host = cmd[1:]
         await hello(host)
-    elif line == "hosts":
-        # TODO: connect to host and list list of hosts
-        pass
-
+    elif cmd == "hosts":
+        m = connect_to_host(default_host)
+        dt = m.local_tasks()
+        r = host_exec_cmd(dt, "get_cluster_info().keys()")
+        print(r)
+    else:
+        print(f"unknown command: {cmd}")
 
 
 async def sac():
     print(">>> ", end='')
     sys.stdout.flush()
     for line in sys.stdin:
-        line = line.rstrip()
-        await sac_cmd(line)
+        cmd = line.rstrip()
+        await sac_cmd(cmd)
         print(">>> ", end='')
         sys.stdout.flush()
 
 
 async def entry():
-    cmd = None if len(sys.argv) == 1 else " ".join(sys.argv[1:])
-    if cmd is not None:
+    global default_host
+    if default_host.startswith("@"):
+        cmd = default_host
+        default_host = default_host[1:]
         await sac_cmd(cmd)
     else:
         await sac()
