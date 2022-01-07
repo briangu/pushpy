a03eccd21cb3db5d9c8fd037e8b4039ca1a3e2c6
Brian Guarraci <brian.guarraci@gmail.com>
2022-01-06 17:05:17 -0700

    simplify launch script

--- push/examples/run_cluster.sh
+++ push/examples/run_cluster.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-$PUSH_HOME/push/examples/run_server.sh GPU 10000 10001 10002 > /tmp/push.10000.txt 2> /tmp/push.10000.err &
-$PUSH_HOME/push/examples/run_server.sh CPU 10001 10000 10002 > /tmp/push.10001.txt 2> /tmp/push.10001.err &
-$PUSH_HOME/push/examples/run_server.sh CPU 10002 10001 10000 > /tmp/push.10002.txt 2> /tmp/push.10002.err &
+$PUSH_HOME/push/examples/run_server.sh GPU localhost:10000 localhost:10001 localhost:10002 > /tmp/push.10000.txt 2> /tmp/push.10000.err &
+$PUSH_HOME/push/examples/run_server.sh CPU localhost:10001 localhost:10000 localhost:10002 > /tmp/push.10001.txt 2> /tmp/push.10001.err &
+$PUSH_HOME/push/examples/run_server.sh CPU localhost:10002 localhost:10001 localhost:10000 > /tmp/push.10002.txt 2> /tmp/push.10002.err &
 
 python3 $PUSH_HOME/push/push_repl.py 50000
 
--- push/examples/run_server.sh
+++ push/examples/run_server.sh
@@ -1,1 +1,2 @@
-python3 $PUSH_HOME/push/push_server.py file:///$PUSH_HOME/push/examples/ex_boot.py $1 localhost:$2 localhost:$3 localhost:$4
+python3 $PUSH_HOME/push/push_server.py file:///$PUSH_HOME/push/examples/ex_boot.py $*
+
